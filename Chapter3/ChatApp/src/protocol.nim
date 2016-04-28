46f4e5dccd7e6df16c6ecb9fc4fe0b50ebd61b90
Dominik Picheta <dominikpicheta@gmail.com>
2016-04-28 15:10:39 +0100

    More comments and nicer code for ChatApp.

--- Chapter3/ChatApp/src/protocol.nim
+++ Chapter3/ChatApp/src/protocol.nim
@@ -1,13 +1,35 @@
 import json
+
 type
   Message* = object
     username*: string
     message*: string
 
-proc parseMessage*(data: string): Message =
-  let dataJson = parseJson(data)
+  MessageParsingError* = object of Exception
+
+proc parseMessage*(data: string): Message {.raises: [MessageParsingError, KeyError].} =
+  var dataJson: JsonNode
+  try:
+    dataJson = parseJson(data)
+  except JsonParsingError:
+    raise newException(MessageParsingError, "Invalid JSON: " &
+                       getCurrentExceptionMsg())
+  except:
+    raise newException(MessageParsingError, "Unknown error: " &
+                       getCurrentExceptionMsg())
+
+  if not dataJson.hasKey("username"):
+    raise newException(MessageParsingError, "Username field missing")
+
   result.username = dataJson["username"].getStr()
+  if result.username.len == 0:
+    raise newException(MessageParsingError, "Username field is empty")
+
+  if not dataJson.hasKey("username"):
+    raise newException(MessageParsingError, "Message field missing")
   result.message = dataJson["message"].getStr()
+  if result.message.len == 0:
+    raise newException(MessageParsingError, "Message field is empty")
 
 proc createMessage*(username, message: string): string =
   result = $(%{
@@ -17,23 +39,17 @@ proc createMessage*(username, message: string): string =
 
 when isMainModule:
   block:
-    let data = """{"username": "John", "message": "Hi!"}"""
+    let data = """{"username": "dom", "message": "hello"}"""
     let parsed = parseMessage(data)
-    doAssert parsed.username == "John"
-    doAssert parsed.message == "Hi!"
+    doAssert parsed.message == "hello"
+    doAssert parsed.username == "dom"
 
+  # Test failure
   block:
-    let data = """foobar"""
     try:
-      let parsed = parseMessage(data)
-      doAssert false
-    except JsonParsingError:
+      let parsed = parseMessage("asdasd")
+    except MessageParsingError:
       doAssert true
     except:
       doAssert false
 
-  block:
-    let expected = """{"username":"dom","message":"hello"}""" & "\c\l"
-    doAssert createMessage("dom", "hello") == expected
-
-  echo("All tests passed!")
